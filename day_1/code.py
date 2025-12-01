6c4a8d4089177e0ff9f838d4c5324cb209a26ba3
Bigmikko <95642308+Bigmikko@users.noreply.github.com>
2025-12-01 19:56:02 +0000

    Added the problem description, comments and a test switch. Removed outputting the dial at each dial turn

--- day_1/code.py
+++ day_1/code.py
@@ -1,28 +1,42 @@
+#Advent of Code 2025
+#Day 1
+#Coded by Bigmikko in Python
+
+#Amount of dial values and where the dial starts
 DIAL_VALUES = 100
 DIAL_START_VALUE = 50
+TEST_INPUT_FILE = "day_1/test_input.txt"
 INPUT_FILE = "day_1/input.txt"
-PART2 = True
 
-def rotate(uinput):
+#Switch between test input and the problem parts
+PART2 = True
+TEST_INPUT = False
+
+#A function that takes the input and "spins" the dial, calculating multiple variables
+def rotate(action):
     global dial
     global password
 
-    if uinput[0] == 'R':
+    #Checks the direction of the dial spin
+    if action[0] == 'R':
         clockwise = True
-    elif uinput[0] == 'L':
+    elif action[0] == 'L':
         clockwise = False
     else:
         print("Invalid Input")
 
-    rotation = int(uinput[1:])
+    #Parses out the numbers after the initial 'R' or 'L'
+    rotation = int(action[1:])
 
     if not clockwise:
         rotation = -rotation
         
+    #Makes sure that you don't count 0 twice when starting on 0
     if dial == 0 and dial + rotation < 0 and PART2:
         password -= 1
     dial += rotation
 
+    #Adds or removes dials numbers until it lands on a valid one, for part two, we count every iteration as passing 0 once
     while dial < 0 or dial >= DIAL_VALUES:
         if dial != 100 and PART2:
             password += 1
@@ -34,12 +48,19 @@ def rotate(uinput):
     if dial == 0:
         password += 1
 
+#Main function starts here
 dial = DIAL_START_VALUE
 password = 0
 
-with open(INPUT_FILE) as f:
-    for line in f:
-        rotate(line)
-        print(dial)
+#Switches between the test and normal inputs
+if TEST_INPUT == True:
+    file = TEST_INPUT_FILE
+else:
+    file = INPUT_FILE
+
+#Reads the file and calls the rotate function for each row in the file
+with open(file) as f:
+    for action in f:
+        rotate(action)
 
 print(password)
